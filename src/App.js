b4290494b5a00911385de1c96a195f6241ce0ba3
ppppbn <phambaongoc1511@gmail.com>
2018-06-01 13:06:09 +0700

    Add gradient color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 const bg = ['bg-primary', 'bg-success', 'bg-warning', 'bg-danger', 'bg-info', 'bg-violet', 'bg-dark'];
+const bgGra = ['bg-1', 'bg-2', 'bg-3', 'bg-4', 'bg-5', 'bg-6', 'bg-7', 'bg-8', 'bg-9', 'bg-10', 'bg-11', 'bg-12'];
 
 class App extends Component {
   constructor(){
@@ -10,7 +11,8 @@ class App extends Component {
       exponent : 0,
       generated : false,
       exponentArray : [],
-      screen : 1
+      screen : 1,
+      gradient : false
     }
   }
 
@@ -40,8 +42,22 @@ class App extends Component {
     }) : 0;
   }
 
+  toggleGradient = () => {
+    this.setState({
+      gradient : !this.state.gradient
+    })
+  }
+
+  colorClass = (val, val2) => {
+    console.log('gra', this.state.gradient);
+    var customColor = this.state.gradient ? 
+      bgGra[(val + val2)%12] : bg[Math.floor(Math.random() * 7)];
+    return customColor + " list-element horizontal-center vertical-center";
+  }
+
   render() {
     let screenClass;
+    const gradient = this.state.gradient;
     switch(this.state.screen) {
       case 1 : 
         screenClass = " custom-display-flex custom-column";
@@ -62,7 +78,7 @@ class App extends Component {
     let lists = this.state.exponentArray.map((val, key) => {
       let row = this.state.exponentArray.slice(0, key + 1).map((val2, key2) => {
         return (
-          <div className={"list-element horizontal-center vertical-center " + bg[Math.floor(Math.random()*7)]}>
+          <div className={this.colorClass(val, val2)}>
             <h4 className="custom-display-inline-block text-white">{this.calculateValue(key, key2)}</h4>
           </div>
         )
@@ -80,8 +96,9 @@ class App extends Component {
           <div className="input-container">
             <form onSubmit={this.generate}>
               <input type="number" className="form-control" placeholder="Enter n?" value={this.state.exponent ? this.state.exponent : ""} onChange={this.changeExponent}></input>
-              <button type="submit" className="btn btn-primary">Generate</button>
-              <button type="button" className="btn btn-success" onClick={this.rotate}>Rotate</button>
+              <button type="submit" className="btn btn-nav btn-primary">Generate</button>
+              <button type="button" className="btn btn-nav btn-success" onClick={this.rotate}>Rotate</button>
+              <button type="button" className="btn btn-nav btn-danger" onClick={this.toggleGradient}>{this.state.gradient ? 'Simple!' : 'Gradient!'}</button>
             </form>
           </div>
         </div>
